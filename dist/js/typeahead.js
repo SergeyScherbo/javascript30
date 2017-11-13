489796fcd1402234f2f80dc861de3037e6d459d4
Sergey Scherbo <sergey31scherbo@gmail.com>
2017-11-13 22:01:09 +0400

    typeahead code

--- dist/js/typeahead.js
+++ dist/js/typeahead.js
@@ -2,5 +2,44 @@
 
 var endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-console.log('hi');
-console.log('sup');
+var cities = [];
+
+fetch(endpoint).then(function (blob) {
+  return blob.json();
+}).then(function (data) {
+  for (var i = 0; i < data.length; i++) {
+    cities.push(data[i]);
+  }
+
+  return cities;
+});
+
+function findMatches(wordToMatch, cities) {
+  return cities.filter(function (place) {
+    var regex = new RegExp(wordToMatch, 'gi');
+    return place.city.match(regex) || place.state.match(regex);
+  });
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function displayMatches() {
+  var _this = this;
+
+  var matchArray = findMatches(this.value, cities);
+  var html = matchArray.map(function (place) {
+    var regex = new RegExp(_this.value, 'gi');
+    var cityName = place.city.replace(regex, '<span class="hl">' + _this.value + '</span>');
+    var stateName = place.state.replace(regex, '<span class="hl">' + _this.value + '</span>');
+    return '\n      <li>\n        <span class="name">' + cityName + ', ' + stateName + '</span>\n        <span class="population">' + numberWithCommas(place.population) + '</span>\n      </li>\n    ';
+  }).join('');
+  suggestions.innerHTML = html;
+}
+
+var searchInput = document.querySelector('.search');
+var suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
