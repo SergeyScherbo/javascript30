632d811b671da868c0efceeacc75b4063b01a87c
Sergey Scherbo <sergey31scherbo@gmail.com>
2017-11-14 22:15:01 +0400

    my solution to the arrays-2 challenge

--- dist/js/arrays-2.js
+++ dist/js/arrays-2.js
@@ -6,12 +6,44 @@ var comments = [{ text: 'Love this!', id: 523423 }, { text: 'Super good', id: 82
 
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
+people.some(function (person) {
+  return person.year < 1998;
+}); // if this is true, that means that person.year is older than 19
 // Array.prototype.every() // is everyone 19 or older?
+people.every(function (person) {
+  return person.year < 1998;
+}); // if this is true, that means that person.year is older than 19, but in this example if we want to get true, we need every single person in the people object to be older than 19 (in this canse we get false);
+
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+comments.find(function (comment) {
+  return comment.id === 823423;
+});
+// I also wanted to make it with for loop
+for (var i = 0; i < comments.length; i++) {
+  if (comments[i].id === 823423) {
+    console.log(comments[i]);
+  }
+}
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
+console.log(comments.findIndex(function (comment) {
+  return comment.id === 823423;
+}));
+// Made it with for loop as well
+for (var _i = 0; _i < comments.length; _i++) {
+  if (comments[_i].id === 823423) {
+    console.log(comments.indexOf(comments[_i]));
+  }
+}
 // delete the comment with the ID of 823423
+var myId = comments.findIndex(function (comment) {
+  return comment.id === 823423;
+});
+if (myId > -1) {
+  comments.splice(myId, 1);
+}
+console.log(comments);
--- src/js/arrays-2.js
+++ src/js/arrays-2.js
@@ -15,12 +15,34 @@ const comments = [
 
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
+people.some(person => person.year < 1998); // if this is true, that means that person.year is older than 19
 // Array.prototype.every() // is everyone 19 or older?
+people.every(person => person.year < 1998); // if this is true, that means that person.year is older than 19, but in this example if we want to get true, we need every single person in the people object to be older than 19 (in this canse we get false);
+
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+comments.find(comment => comment.id === 823423);
+// I also wanted to make it with for loop
+for (let i = 0; i < comments.length; i++) {
+  if (comments[i].id === 823423) {
+    console.log(comments[i]);
+  }
+}
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
+console.log(comments.findIndex(comment => comment.id === 823423));
+// Made it with for loop as well
+for (let i = 0; i < comments.length; i++) {
+  if (comments[i].id === 823423) {
+    console.log(comments.indexOf(comments[i]));
+  }
+}
 // delete the comment with the ID of 823423
+const myId = comments.findIndex(comment => comment.id === 823423);
+if (myId > -1) {
+  comments.splice(myId, 1);
+}
+console.log(comments);
