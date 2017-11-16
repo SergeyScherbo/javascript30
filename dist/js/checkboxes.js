9d7d8676269a51a43b85c6c76db2779fbbfbf9ef
Sergey Scherbo <sergey31scherbo@gmail.com>
2017-11-16 22:28:32 +0400

    checkboxes code

--- dist/js/checkboxes.js
+++ dist/js/checkboxes.js
@@ -1,3 +1,26 @@
 'use strict';
 
 var checkboxes = document.querySelectorAll('input');
+var lastChecked = void 0;
+
+function handleCheck(e) {
+  var _this = this;
+
+  var inBetween = false;
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach(function (checkbox) {
+      if (checkbox === _this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+  lastChecked = this;
+}
+
+checkboxes.forEach(function (checkbox) {
+  return checkbox.addEventListener('click', handleCheck);
+});
