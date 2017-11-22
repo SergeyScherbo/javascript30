4059a6caf2898b7c5bf44de4a860003bbfdc41e2
Sergey Scherbo <sergey31scherbo@gmail.com>
2017-11-22 23:06:38 +0400

    textshadow code

--- dist/js/textshadow.js
+++ dist/js/textshadow.js
@@ -1,3 +1,27 @@
 'use strict';
 
-console.log('hello');
+var hero = document.querySelector('.hero');
+var text = hero.querySelector('h1');
+var walk = 50;
+
+function shadow(e) {
+  var width = hero.offsetWidth;
+  var height = hero.offsetHeight;
+
+  var x = e.offsetX;
+  var y = e.offsetY;
+
+  if (this !== e.target) {
+    x += e.target.offsetLeft;
+    y += e.target.offsetTop;
+  }
+
+  var xWalk = Math.round(x / width * walk - walk / 2);
+  var yWalk = Math.round(y / height * walk - walk / 2);
+
+  console.log(xWalk, yWalk);
+
+  text.style.textShadow = '\n    ' + xWalk + 'px ' + yWalk + 'px 0 #259F6C,\n    ' + xWalk * -1 + 'px ' + yWalk + 'px 0 #EF7E56,\n    ' + xWalk + 'px ' + yWalk * -1 + 'px 0 #3F6699\n  ';
+}
+
+hero.addEventListener('mousemove', shadow);
--- src/js/textshadow.js
+++ src/js/textshadow.js
@@ -1,1 +1,29 @@
-console.log('hello');
+const hero = document.querySelector('.hero');
+const text = hero.querySelector('h1');
+const walk = 50;
+
+function shadow(e) {
+  const width = hero.offsetWidth;
+  const height = hero.offsetHeight;
+
+  let x = e.offsetX;
+  let y = e.offsetY;
+
+  if (this !== e.target) {
+    x += e.target.offsetLeft;
+    y += e.target.offsetTop;
+  }
+
+  const xWalk = Math.round((x / width * walk) - (walk / 2));
+  const yWalk = Math.round((y / height * walk) - (walk / 2));
+
+  console.log(xWalk, yWalk);
+
+  text.style.textShadow = `
+    ${xWalk}px ${yWalk}px 0 #259F6C,
+    ${xWalk * -1}px ${yWalk}px 0 #EF7E56,
+    ${xWalk}px ${yWalk * -1}px 0 #3F6699
+  `;
+}
+
+hero.addEventListener('mousemove', shadow);
