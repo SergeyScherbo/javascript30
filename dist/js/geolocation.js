ccec336645c676f0b3acf2881a3335798cb3073d
Sergey Scherbo <sergey31scherbo@gmail.com>
2017-11-25 21:30:59 +0400

    geolocation code

--- dist/js/geolocation.js
+++ dist/js/geolocation.js
@@ -1,1 +1,12 @@
-"use strict";
+'use strict';
+
+var arrow = document.querySelector('.arrow');
+var speed = document.querySelector('.speed-value');
+
+navigator.geolocation.watchPosition(function (data) {
+  speed.textContent = Math.round(data.coords.speed);
+  arrow.style.transform = 'rotate(' + data.coords.heading + 'deg)';
+}, function (err) {
+  console.log(err);
+  alert('Hey, you gotta allow that to happen!');
+});
