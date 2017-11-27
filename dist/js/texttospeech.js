3a16f989573c1b017f1c99e7da251f32e625a530
Sergey Scherbo <sergey31scherbo@gmail.com>
2017-11-27 22:06:19 +0400

    text-to-speech code

--- dist/js/texttospeech.js
+++ dist/js/texttospeech.js
@@ -6,3 +6,46 @@ var voicesDropdown = document.querySelector('[name="voice"]');
 var options = document.querySelectorAll('[type="range"], [name="text"]');
 var speakButton = document.querySelector('#speak');
 var stopButton = document.querySelector('#stop');
+
+function populateVoices() {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices.filter(function (voice) {
+    return voice.lang.includes('en');
+  }).map(function (voice) {
+    return '<option value="' + voice.name + '">' + voice.name + ' (' + voice.lang + ')</option>';
+  }).join('');
+}
+
+function setVoice() {
+  var _this = this;
+
+  msg.voice = voices.find(function (voice) {
+    return voice.name === _this.value;
+  });
+  toggle();
+}
+
+function toggle() {
+  var startOver = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : true;
+
+  speechSynthesis.cancel();
+  if (startOver) {
+    speechSynthesis.speak(msg);
+  }
+}
+
+function setOption() {
+  console.log(this.name, this.value);
+  msg[this.name] = this.value;
+  toggle();
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
+options.forEach(function (option) {
+  return option.addEventListener('change', setOption);
+});
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', function () {
+  return toggle(false);
+});
