e83c92e299c58783f4375b5021a73f41d1fd4f56
Sergey Scherbo <sergey31scherbo@gmail.com>
2017-11-29 23:26:12 +0400

    events code

--- dist/js/events.js
+++ dist/js/events.js
@@ -1,1 +1,16 @@
-"use strict";
+'use strict';
+
+var divs = document.querySelectorAll('div');
+
+function logText(e) {
+  console.log(this.classList.value);
+  // e.stopPropagation();
+  // console.log(this);
+}
+
+divs.forEach(function (div) {
+  return div.addEventListener('click', logText, {
+    capture: true,
+    once: true
+  });
+});
