7d87765b521ea3f22cf35c69a59b1348634a77a0
Sergey Scherbo <sergey31scherbo@gmail.com>
2017-11-30 23:04:26 +0400

    follow-nav code

--- dist/js/follownav.js
+++ dist/js/follownav.js
@@ -1,1 +1,43 @@
-"use strict";
+'use strict';
+
+var triggers = document.querySelectorAll('.cool > li');
+var background = document.querySelector('.dropdownBackground');
+var nav = document.querySelector('.top');
+
+function handleEnter() {
+  var _this = this;
+
+  this.classList.add('trigger-enter');
+  setTimeout(function () {
+    if (_this.classList.contains('trigger-enter')) {
+      _this.classList.add('trigger-enter-active');
+    }
+  }, 150);
+  background.classList.add('open');
+
+  var dropdown = this.querySelector('.dropdown');
+
+  var dropdownCoords = dropdown.getBoundingClientRect();
+  var navCoords = nav.getBoundingClientRect();
+
+  var coords = {
+    height: dropdownCoords.height,
+    width: dropdownCoords.width,
+    top: dropdownCoords.top - navCoords.top,
+    left: dropdownCoords.left - navCoords.left
+  };
+
+  background.style.setProperty('height', coords.height + 'px');
+  background.style.setProperty('width', coords.width + 'px');
+  background.style.setProperty('transform', 'translate(' + coords.left + 'px, ' + coords.top + 'px)');
+}
+
+function handleLeave() {
+  this.classList.remove('trigger-enter', 'trigger-enter-active');
+  background.classList.remove('open');
+}
+
+triggers.forEach(function (trigger) {
+  trigger.addEventListener('mouseenter', handleEnter);
+  trigger.addEventListener('mouseleave', handleLeave);
+});
