e65248f6b36112553a3ac3d317ba747686ea15e5
Sergey Scherbo <sergey31scherbo@gmail.com>
2017-12-01 23:26:06 +0400

    drag-to-scroll code

--- dist/js/dragtoscroll.js
+++ dist/js/dragtoscroll.js
@@ -1,1 +1,31 @@
-"use strict";
+'use strict';
+
+var slider = document.querySelector('.items');
+var isDown = false;
+var startX = void 0;
+var scrollLeft = void 0;
+
+slider.addEventListener('mousedown', function (e) {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', function () {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', function () {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', function (e) {
+  if (!isDown) return;
+  e.preventDefault();
+  var x = e.pageX - slider.offsetLeft;
+  var walk = x - startX;
+  slider.scrollLeft = scrollLeft - walk;
+});
